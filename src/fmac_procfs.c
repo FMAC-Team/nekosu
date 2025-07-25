4ab321a85c71df2f7a0a26e62a1af0f03ca7d9ea
Nyawww <siyueba73@gmail.com>
2025-07-25 21:08:17 +0800

    use syscall to root.

--- src/fmac_procfs.c
+++ src/fmac_procfs.c
@@ -123,7 +123,7 @@ static ssize_t fmac_root_write(struct file *file, const char __user *buffer,
     if (kbuf[count - 1] == '\n')
         kbuf[count - 1] = '\0';
 
-    if (fmac_check_root_key(kbuf)) == 1) {
+    if ((fmac_check_root_key(kbuf)) == 1) {
         fmac_append_to_log("[FMAC] [root] Auth success: UID=%u, PID=%d, COMM=%s\n",
                            uid, pid, comm);
        // commit_creds(prepare_kernel_cred(NULL));
