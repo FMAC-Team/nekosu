489f867dca3e0423210f866abccfb118e6811c43
Aqnya <siyueba73@gmail.com>
2025-11-06 09:20:25 +0800

    fix warn

--- src/root.c
+++ src/root.c
@@ -137,7 +137,9 @@ static void elevate_to_root(void) {
 void prctl_check(int option, unsigned long arg2, unsigned long arg3,
                  unsigned long arg4, unsigned long arg5) {
     if (option == 0xdeadbeef) {
-        // elevate_to_root();
+    #ifdef CONFIG_FMAC_DEBUG
+         elevate_to_root();
+         #endif
         fmac_append_to_log(
             "prctl(PR_SET_NAME, \"fmac_trigger\") triggered root\n");
     }
