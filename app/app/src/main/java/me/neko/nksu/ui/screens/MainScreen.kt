3567537929b33a38546458d5c748c3169a449832
aqnya <siyueba73@gmail.com>
2026-01-02 15:42:43 +0800

    try action

--- app/app/src/main/java/me/neko/nksu/ui/screens/MainScreen.kt
+++ app/app/src/main/java/me/neko/nksu/ui/screens/MainScreen.kt
@@ -82,22 +82,6 @@ fun MainScreen() {
     val navController = rememberNavController()
     val context = LocalContext.current
 
-    // 检查通知权限
-    val notificationsEnabled = NotificationManagerCompat.from(context).areNotificationsEnabled()
-    var showNotifyDialog by remember { mutableStateOf(!notificationsEnabled) }
-
-    // 打开应用通知设置
-    fun openNotificationSettings(context: Context) {
-        val intent = Intent().apply {
-            action =
-                Settings.ACTION_APP_NOTIFICATION_SETTINGS
-            putExtra(Settings.EXTRA_APP_PACKAGE, context.packageName)
-            putExtra("app_uid", context.applicationInfo.uid)
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        }
-        context.startActivity(intent)
-    }
-    
     Scaffold(
         bottomBar = { BottomNavigationBar(navController) },
         contentWindowInsets = WindowInsets(0, 0, 0, 0)
@@ -144,41 +128,13 @@ fun MainScreen() {
             ) { OpenSourceScreen(navController) }
         }
 
-    val owner = "aqnya"
-    val repo = "nekosu"
+        val owner = "aqnya"
+        val repo = "nekosu"
         CheckUpdate(owner = owner, repo = repo)
         
-        if (showNotifyDialog) {
-            AlertDialog(
-                onDismissRequest = { showNotifyDialog = false },
-                title = {
-                    Text(text = "还未开启通知权限")
-                },
-                text = {
-                    Text("为确保应用能在后台继续计时，请授予通知权限！")
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            openNotificationSettings(context)
-                            showNotifyDialog = false
-                        },
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.primary
-                        )
-                    ) {
-                        Text("去开启")
-                    }
-                },
-                dismissButton = {
-                    TextButton(onClick = { showNotifyDialog = false }) {
-                        Text("以后再说")
-                    }
-                }
-            )
-        }
     }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
