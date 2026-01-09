6fc08b56b35b695fa20fb2a3d91a77ab542f1646
aqnya <siyueba73@gmail.com>
2026-01-09 20:35:22 +0800

    fix sigdata log

--- app/app/src/main/cpp/libnksu.cpp
+++ app/app/src/main/cpp/libnksu.cpp
@@ -1,41 +1,35 @@
+#include <iostream>
 #include <jni.h>
 #include <string>
-#include <iostream>
 
 #include "log.hpp"
 #include "su.hpp"
 
-extern "C"
-JNIEXPORT jint JNICALL
-Java_me_neko_nksu_Native_authenticate(
-        JNIEnv* env,
-        jobject /* this */,
-        jstring jKey,
-        jstring jToken) {
+extern "C" JNIEXPORT jint JNICALL Java_me_neko_nksu_Native_authenticate(
+    JNIEnv *env, jobject /* this */, jstring jKey, jstring jToken) {
 
+  const char *nativeKey = env->GetStringUTFChars(jKey, nullptr);
+  const char *nativeToken = env->GetStringUTFChars(jToken, nullptr);
 
-    const char *nativeKey = env->GetStringUTFChars(jKey, nullptr);
-    const char *nativeToken = env->GetStringUTFChars(jToken, nullptr);
+  std::string key(nativeKey);
+  std::string token(nativeToken);
 
-    std::string key(nativeKey);
-    std::string token(nativeToken);
+  int result = 0;
 
-    int result = 0;
-
-    // 3. 校验逻辑
-    if (key.empty() || token.empty()) {
-        LOGE("need args");
-        result = -2;
-    } else {
-        if (AuthenticationManager(key, token) == -1) {
-            LOGE("AuthenticationManager falied!");
-            print_openssl_errors();
-            result = -1;
-        }
+  // 3. 校验逻辑
+  if (key.empty() || token.empty()) {
+    LOGE("need args");
+    result = -2;
+  } else {
+    if (AuthenticationManager(key, token) == -1) {
+      LOGE("AuthenticationManager falied!");
+      print_openssl_errors();
+      result = -1;
     }
+  }
 
-    env->ReleaseStringUTFChars(jKey, nativeKey);
-    env->ReleaseStringUTFChars(jToken, nativeToken);
+  env->ReleaseStringUTFChars(jKey, nativeKey);
+  env->ReleaseStringUTFChars(jToken, nativeToken);
 
-    return result; 
+  return result;
 }
--- app/app/src/main/cpp/su.cpp
+++ app/app/src/main/cpp/su.cpp
@@ -16,61 +16,60 @@ int prctl(int option, void *arg2, void *arg3, void *arg4, void *arg5) {
   // option -> code
   // arg2 -> address
   // arg3 -> lenght
-  LOGI("option: %x\narg2: %p\narg3: %zu\n",option,arg2,(size_t)arg3);
+  LOGI("option: %x\narg2: %p\narg3: %zu\n", option, arg2, (size_t)arg3);
   return syscall(SYS_prctl, option, arg2, arg3, arg4, arg5);
 }
 
 int get_root(void) {
-int uid1=getuid();
-    prctl(GE_ROOT,NULL,NULL,NULL,NULL);
-if(uid1 == getuid()){
+  int uid1 = getuid();
+  prctl(GE_ROOT, NULL, NULL, NULL, NULL);
+  if (uid1 == getuid()) {
     LOGE("Permission denied");
     return -1;
-}else{
+  } else {
     return 0;
+  }
 }
-}
 
-
-std::vector<unsigned char> sign_data(const std::string &data, EVP_PKEY *priv_key) {
-    std::vector<unsigned char> sig;
-    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
-    if (!ctx) {
-        ERR_print_errors_fp(stderr);
-        return sig;
-    }
-    bool success = false;
-    do {
-        if (1 != EVP_DigestSignInit(ctx, nullptr, EVP_sha256(), nullptr, priv_key)) {
-            break;
-        }
-        if (1 != EVP_DigestSignUpdate(ctx, data.data(), data.size())) {
-            break;
-        }
-        size_t sig_len = 0;
-        if (1 != EVP_DigestSignFinal(ctx, nullptr, &sig_len) || sig_len <= 0) {
-            break;
-        }
-        sig.resize(sig_len);
-        if (1 != EVP_DigestSignFinal(ctx, sig.data(), &sig_len)) {
-            sig.clear();
-            break;
-        }
-        sig.resize(sig_len);
-        success = true;
-
-    } while (false);
-
-    if (!success) {
-        ERR_print_errors_fp(stderr);
-    }
-    EVP_MD_CTX_free(ctx);
+std::vector<unsigned char> sign_data(const std::string &data,
+                                     EVP_PKEY *priv_key) {
+  std::vector<unsigned char> sig;
+  EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+  if (!ctx) {
+    ERR_print_errors_fp(stderr);
     return sig;
+  }
+  bool success = false;
+  do {
+    if (1 !=
+        EVP_DigestSignInit(ctx, nullptr, EVP_sha256(), nullptr, priv_key)) {
+      break;
+    }
+    if (1 != EVP_DigestSignUpdate(ctx, data.data(), data.size())) {
+      break;
+    }
+    size_t sig_len = 0;
+    if (1 != EVP_DigestSignFinal(ctx, nullptr, &sig_len) || sig_len <= 0) {
+      break;
+    }
+    sig.resize(sig_len);
+    if (1 != EVP_DigestSignFinal(ctx, sig.data(), &sig_len)) {
+      sig.clear();
+      break;
+    }
+    sig.resize(sig_len);
+    success = true;
+
+  } while (false);
+
+  if (!success) {
+    ERR_print_errors_fp(stderr);
+  }
+  EVP_MD_CTX_free(ctx);
+  return sig;
 }
 
-
- int AuthenticationManager(const std::string key,
-                                 const std::string totp) {
+int AuthenticationManager(const std::string key, const std::string totp) {
   FILE *fp = fopen(key.c_str(), "r");
   if (!fp) {
     LOGE("Can't open ecc key");
@@ -89,10 +88,6 @@ std::vector<unsigned char> sign_data(const std::string &data, EVP_PKEY *priv_key
 
   std::vector<unsigned char> signature = sign_data(totp.c_str(), priv_key);
 
-  LOGI("Success!\nsize: %zu bytes\n", signature.size());
-  printf("signed: ");
-  std::vector<unsigned char> signature = sign_data(totp.c_str(), priv_key);
-
   std::string sig_hex;
   sig_hex.reserve(signature.size() * 2);
   for (unsigned char b : signature) {
@@ -103,7 +98,7 @@ std::vector<unsigned char> sign_data(const std::string &data, EVP_PKEY *priv_key
 
   LOGI("Sig success! size: %zu bytes", signature.size());
   LOGI("signed: %s", sig_hex.c_str());
-  prctl(AU_MANAGER,&signature,(void*)signature.size(),NULL,NULL);
+  prctl(AU_MANAGER, &signature, (void *)signature.size(), NULL, NULL);
   LOGI("please check manager!");
   EVP_PKEY_free(priv_key);
   return get_root();
