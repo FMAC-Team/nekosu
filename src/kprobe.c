a0ae6b020a6e5fd82e7b73e86f3b47a8e6186bd2
aqnya <siyueba73@gmail.com>
2026-01-27 21:14:13 +0800

    fix abi change.

--- src/kprobe.c
+++ src/kprobe.c
@@ -14,6 +14,7 @@
 #include <linux/file.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
+#include <linux/mm.h>
 #include <fmac.h>
 
 static char *shared_buffer;
@@ -26,7 +27,11 @@ static int anon_mmap(struct file *file, struct vm_area_struct *vma)
     if (size > SHM_SIZE)
         return -EINVAL;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
+    vma_set_flags(vma, VM_SHARED | VM_DONTEXPAND | VM_DONTDUMP);
+#else
     vma->vm_flags |= VM_SHARED | VM_DONTEXPAND | VM_DONTDUMP;
+#endif
 
     return remap_vmalloc_range(vma, shared_buffer, 0);
 }
