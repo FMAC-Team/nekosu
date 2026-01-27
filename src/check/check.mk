5ba3af7074cc10ae9ecc4fe2908a23cfd0a80136
aqnya <siyueba73@gmail.com>
2026-01-27 23:01:45 +0800

    fix abi error.

--- src/check/check.mk
+++ src/check/check.mk
@@ -1,19 +1,16 @@
 KDIR := $(KDIR)
 PWD := $(shell pwd)
+CONFIG_H := $(PWD)/config.h
 
 CHECK_FUNCS := vma_set_flags get_user_pages
-CONFIG_H := $(PWD)/config.h
-CHECK_PROGRAM := $(PWD)/function
 
 all: $(CONFIG_H)
 
-$(CHECK_PROGRAM): function.c
-	@echo "Starting function check..."
-	@$(MAKE) -C $(KDIR) M=$(PWD) modules >/dev/null 2>&1 || touch $(CHECK_PROGRAM)
-
-$(CONFIG_H): $(CHECK_PROGRAM)
-	@echo "Generating $(CONFIG_H) ..."
-	@./$(CHECK_PROGRAM) > $(CONFIG_H) || echo "/* No functions detected */" > $(CONFIG_H)
+$(CONFIG_H): function.c
+	@echo "Checking functions..."
+	@$(MAKE) -C $(KDIR) M=$(PWD) modules >/dev/null 2>&1 && \
+	    echo "#define HAVE_vma_set_flags 1" > $(CONFIG_H) || \
+	    echo "/* No functions detected */" > $(CONFIG_H)
 
 clean:
-	rm -f $(CONFIG_H) $(CHECK_PROGRAM)
+	rm -f $(CONFIG_H)
