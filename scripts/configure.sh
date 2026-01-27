ac8d77b5696832bc51db601595476d35a1bf16c7
aqnya <siyueba73@gmail.com>
2026-01-28 01:21:24 +0800

    check configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -8,6 +8,9 @@ CHECK_FUNCS=("printk" "vma_set_flags" "get_user_pages")
 echo "CC $CC"
 echo "CFLAGS $CFLAGS"
 
+KDIR=$(echo "$CFLAGS" | grep -o '\-I[^ ]*' | head -1 | sed 's|-I||' | sed 's|/include$||')
+ARCH=$(echo "$CFLAGS" | grep -o 'arch/[^/]*' | head -1 | cut -d'/' -f2)
+
 echo "#ifndef _NKSU_FUNC_CHECK_H" > "$CONFIG_H"
 echo "#define _NKSU_FUNC_CHECK_H" >> "$CONFIG_H"
 
@@ -15,7 +18,7 @@ echo "Checking kernel functions..."
 
 for FUNC in "${CHECK_FUNCS[@]}"; do
     
-    cat <<EOF | $CC $CFLAGS -Wno-unused -Werror=implicit-function-declaration -xc - -c -o /dev/null #>/dev/null 2>&1
+    cat <<EOF | $CC $CFLAGS -I${KDIR}/include/generated -I${KDIR}/include/generated/uapi -I${KDIR}/arch/${ARCH}/include/generated -I${KDIR}/arch/${ARCH}/include/generated/uapi -Wno-unused -Werror=implicit-function-declaration -xc - -c -o /dev/null 2>&1
 #include <linux/module.h>
 #include <linux/mm.h>
 #include <linux/sched.h>
