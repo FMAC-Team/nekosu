605687c04ad1d3fb82e108ab629aa4a24ca5c3bf
aqnya <siyueba73@gmail.com>
2026-01-28 02:06:59 +0800

    check configure.sh

--- src/kprobe.c
+++ src/kprobe.c
@@ -30,7 +30,7 @@ static int anon_mmap(struct file *file, struct vm_area_struct *vma)
 /*#if (LINUX_VERSION_CODE > KERNEL_VERSION(6, 1, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0))
     */
 
-#ifdef HAVE_vma_set_flags
+#if HAVE_vma_set_flags
 vma_set_flags(vma, VM_SHARED | VM_DONTEXPAND | VM_DONTDUMP);
 #else
     vma->vm_flags |= (VM_SHARED | VM_DONTEXPAND | VM_DONTDUMP);
