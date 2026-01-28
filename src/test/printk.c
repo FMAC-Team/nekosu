9117abb5697b0026abcecb439e0a5a0be64dc942
aqnya <siyueba73@gmail.com>
2026-01-28 14:19:45 +0800

    remove build test.

--- src/test/printk.c
+++
@@ -1,8 +0,0 @@
-#include <linux/printk.h>
-#include <linux/module.h>
-
-MODULE_LICENSE("test");
-
-void check_printk(void){
-(void)printk(KERN_INFO "test\n");
-}
--- src/test/vm_flags_set.c
+++
@@ -1,9 +0,0 @@
-#include <linux/mm.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
-
-MODULE_LICENSE("test");
-
-void check_func(void) {
-(void)&vm_flags_set;
-}
