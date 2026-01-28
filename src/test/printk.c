60a82671c9a1a811822588f710f3c16f1909c338
aqnya <siyueba73@gmail.com>
2026-01-28 13:10:31 +0800

    renew test code.

--- src/test/printk.c
+++ src/test/printk.c
@@ -1,5 +1,7 @@
 #include <linux/printk.h>
 
+MODULE_LICENSE("test")
+
 void check_printk(void){
 (void)printk(KERN_INFO "test\n");
 }
--- src/test/vma_flags_set.c
+++ src/test/vma_flags_set.c
@@ -1,6 +1,8 @@
 #include <linux/mm.h>
 #include <linux/kernel.h>
 
+MODULE_LICENSE("test")
+
 void check_func(void) {
 (void)&vma_flags_set;
 }
