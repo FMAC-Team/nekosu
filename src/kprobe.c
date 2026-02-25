d1e6c24abc89d792def2fd3ebb4999af6658031e
aqnya <siyueba73@gmail.com>
2026-02-25 23:53:12 +0800

    fixed regs.

--- src/kprobe.c
+++ src/kprobe.c
@@ -24,7 +24,7 @@ struct {
 static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
 	int fd;
-    unsigned long option, arg2, arg3;
+	unsigned long option, arg2, arg3;
 	struct pt_regs *real_regs;
 
 #if defined(__aarch64__)
@@ -40,25 +40,21 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 
 #if defined(__aarch64__)
 	option = real_regs->regs[0];
-	arg2   = real_regs->regs[1];
-	arg3   = real_regs->regs[2];
+	arg2 = real_regs->regs[1];
+	arg3 = real_regs->regs[2];
 #elif defined(__x86_64__)
 	option = real_regs->di;
-	arg2   = real_regs->si;
-	arg3   = real_regs->dx;
+	arg2 = real_regs->si;
+	arg3 = real_regs->dx;
 #endif
 
-
 	if ((int)option != 201) {
-		if ((current_euid().val) == 10142) {
-			pr_err("option failed: %lx", option);
-		}
 		return 0;
 	}
 
-	/*  if (!access_ok((void __user *)arg3, sizeof(int)))
-	   return 0;
-	 */
+	if (!access_ok((void __user *)arg3, sizeof(int)))
+		return 0;
+
 	pr_alert("prctl hit: option=0x%lx arg2=0x%lx arg3=0x%lx\n", option,
 		 arg2, arg3);
 
