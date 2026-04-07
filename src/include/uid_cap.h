58f8b8338bb479c4cc468d96f6e239ad1ce99b68
aqnya <siyueba73@gmail.com>
2026-04-08 00:33:25 +0800

    add caplist.

---
+++ src/include/uid_cap.h
@@ -0,0 +1,33 @@
+// uid_caps.h
+#ifndef UID_CAPS_H
+#define UID_CAPS_H
+
+#include <linux/hashtable.h>
+#include <linux/spinlock.h>
+#include <linux/types.h>
+
+#define UID_CAPS_HASH_BITS 8
+
+typedef struct {
+    uid_t uid;
+    uint64_t caps;
+    struct hlist_node node;
+} uid_cap_entry_t;
+
+typedef struct {
+    DECLARE_HASHTABLE(table, UID_CAPS_HASH_BITS);
+    spinlock_t lock;
+} uid_caps_table_t;
+
+extern uid_caps_table_t g_uid_caps_table;
+
+int uid_caps_init(void);
+void uid_caps_exit(void);
+int uid_caps_add(uid_t uid, uint64_t caps);
+int uid_caps_remove(uid_t uid);
+int uid_caps_update(uid_t uid, uint64_t caps);
+uint64_t uid_caps_get(uid_t uid);
+int uid_caps_has_uid(uid_t uid);
+int uid_caps_clear_all(void);
+
+#endif // UID_CAPS_H
