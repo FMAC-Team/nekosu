e95471a39d173202ee81a4329780a074c86901ca
aqnya <siyueba73@gmail.com>
2026-04-16 01:54:46 +0800

    add domain.c

--- src/selinux/domain.c
+++ src/selinux/domain.c
@@ -1,39 +1,23 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/errno.h>
-#include <linux/jhash.h>
 #include "ss/policydb.h"
 #include "ss/services.h"
 #include "ss/hashtab.h"
 #include "security.h"
 
-static u32 symhash(const void *key)
-{
-	const char *s = key;
-	return jhash(s, strlen(s), 0);
-}
-
-static int symcmp(const void *key1, const void *key2)
-{
-	return strcmp(key1, key2);
-}
-
-static const struct hashtab_key_params sym_params = {
-	.hash = symhash,
-	.cmp = symcmp,
-};
-
-static int add_type_to_attr(struct policydb *p, const char *type_name,
-			    const char *attr_name)
+static int add_type_to_attr(struct policydb *p, const char *type_name, const char *attr_name)
 {
 	struct type_datum *type;
 	struct type_datum *attr;
 
-	type = hashtab_search(&p->p_types.table, type_name, sym_params);
-	attr = hashtab_search(&p->p_types.table, attr_name, sym_params);
+	type = symtab_search(&p->p_types, type_name);
+	attr = symtab_search(&p->p_types, attr_name);
 
-	if (!type || !attr)
+	if (!type || !attr) {
+		pr_err("ncore: failed to find type(%s) or attr(%s)\n", type_name, attr_name);
 		return -EINVAL;
+	}
 
 	if (p->type_attr_map_array) {
 		ebitmap_set_bit(&p->type_attr_map_array[type->value - 1],
@@ -47,14 +31,12 @@ static int add_type_to_policy(struct policydb *p, const char *name)
 {
 	struct type_datum *type;
 	char *name_copy;
-	int rc;
 	uint32_t new_value;
+	void *tmp;
+	int i;
 
-	void *tmp_names;
-	void *tmp_structs;
-	void *tmp_map;
-
-	if (hashtab_search(&p->p_types.table, name, sym_params))
+	type = symtab_search(&p->p_types, name);
+	if (type)
 		return 0;
 
 	type = kzalloc(sizeof(*type), GFP_KERNEL);
@@ -71,31 +53,33 @@ static int add_type_to_policy(struct policydb *p, const char *name)
 	type->primary = 1;
 	type->value = new_value;
 
-	tmp_names = krealloc(p->sym_val_to_name[SYM_TYPES],
+	tmp = krealloc(p->sym_val_to_name[SYM_TYPES],
 			     sizeof(char *) * new_value, GFP_KERNEL);
-	if (!tmp_names)
-		goto err;
-	p->sym_val_to_name[SYM_TYPES] = tmp_names;
+	if (!tmp) goto err;
+	p->sym_val_to_name[SYM_TYPES] = tmp;
 	p->sym_val_to_name[SYM_TYPES][new_value - 1] = name_copy;
 
-	tmp_structs = krealloc(p->type_val_to_struct,
+	tmp = krealloc(p->type_val_to_struct,
 			       sizeof(struct type_datum *) * new_value,
 			       GFP_KERNEL);
-	if (!tmp_structs)
-		goto err;
-	p->type_val_to_struct = tmp_structs;
+	if (!tmp) goto err;
+	p->type_val_to_struct = tmp;
 	p->type_val_to_struct[new_value - 1] = type;
 
-	tmp_map = krealloc(p->type_attr_map_array,
+	tmp = krealloc(p->type_attr_map_array,
 			   sizeof(struct ebitmap) * new_value, GFP_KERNEL);
-	if (!tmp_map)
-		goto err;
-	p->type_attr_map_array = tmp_map;
+	if (!tmp) goto err;
+	p->type_attr_map_array = tmp;
 	ebitmap_init(&p->type_attr_map_array[new_value - 1]);
+	
+	ebitmap_set_bit(&p->type_attr_map_array[new_value - 1], new_value - 1, 1);
 
-	rc = hashtab_insert(&p->p_types.table, name_copy, type, sym_params);
-	if (rc)
+	if (symtab_insert(&p->p_types, name_copy, type))
 		goto err;
+
+	for (i = 0; i < p->p_roles.nprim; ++i) {
+		ebitmap_set_bit(&p->role_val_to_struct[i]->types, new_value - 1, 1);
+	}
 
 	p->p_types.nprim++;
 	return 0;
