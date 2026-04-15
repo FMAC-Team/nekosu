ea2c9025d1dda8a1d33f243383eb70cea7ad5811
aqnya <siyueba73@gmail.com>
2026-04-15 16:50:30 +0800

    fix value.

--- src/selinux/rule.c
+++ src/selinux/rule.c
@@ -11,6 +11,44 @@
 #include "security.h"
 #include "avc_ss.h"
 
+static inline int avtab_hash(const struct avtab_key *keyp, u32 mask)
+{
+	static const u32 c1 = 0xcc9e2d51;
+	static const u32 c2 = 0x1b873593;
+	static const u32 r1 = 15;
+	static const u32 r2 = 13;
+	static const u32 m  = 5;
+	static const u32 n  = 0xe6546b64;
+
+	u32 hash = 0;
+
+#define mix(input) { \
+	u32 v = input; \
+	v *= c1; \
+	v = (v << r1) | (v >> (32 - r1)); \
+	v *= c2; \
+	hash ^= v; \
+	hash = (hash << r2) | (hash >> (32 - r2)); \
+	hash = hash * m + n; \
+}
+
+	mix(keyp->target_class);
+	mix(keyp->target_type);
+	mix(keyp->source_type);
+
+#undef mix
+
+	hash ^= hash >> 16;
+	hash *= 0x85ebca6b;
+	hash ^= hash >> 13;
+	hash *= 0xc2b2ae35;
+	hash ^= hash >> 16;
+
+	return hash & mask;
+}
+
+
+
 extern struct selinux_state selinux_state;
 
 static struct policydb *fmac_get_pdb(void)
