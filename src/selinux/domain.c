137bc1a281815f3e813b4a1b01b6fd34440532b1
aqnya <siyueba73@gmail.com>
2026-04-16 01:21:09 +0800

    add domain.c

--- src/selinux/domain.c
+++ src/selinux/domain.c
@@ -7,10 +7,10 @@
 #include "ss/hashtab.h"
 #include "security.h"
 
-static u32 symhash(const void *key, u32 size)
+static u32 symhash(const void *key)
 {
     const char *s = key;
-    return jhash(s, strlen(s), 0) & (size - 1);
+    return jhash(s, strlen(s), 0);
 }
 
 static int symcmp(const void *key1, const void *key2)
@@ -28,14 +28,12 @@ static int add_type_to_attr(struct policydb *p, const char *type_name, const cha
     struct type_datum *type;
     struct type_datum *attr;
 
-    type = hashtab_search(p->p_types.table, type_name, sym_params);
-    attr = hashtab_search(p->p_types.table, attr_name, sym_params);
+    type = hashtab_search(&p->p_types.table, type_name, sym_params);
+    attr = hashtab_search(&p->p_types.table, attr_name, sym_params);
 
     if (!type || !attr)
         return -EINVAL;
-
-    ebitmap_set_bit(&attr->types, type->value - 1, 1);
-
+    
     if (p->type_attr_map_array) {
         ebitmap_set_bit(&p->type_attr_map_array[type->value - 1], attr->value - 1, 1);
     }
@@ -49,8 +47,12 @@ static int add_type_to_policy(struct policydb *p, const char *name)
     char *name_copy;
     int rc;
     uint32_t new_value;
+    
+    void *tmp_names;
+    void *tmp_structs;
+    void *tmp_map;
 
-    if (hashtab_search(p->p_types.table, name, sym_params))
+    if (hashtab_search(&p->p_types.table, name, sym_params))
         return 0;
 
     type = kzalloc(sizeof(*type), GFP_KERNEL);
@@ -67,25 +69,25 @@ static int add_type_to_policy(struct policydb *p, const char *name)
     type->primary = 1;
     type->value = new_value;
 
-    void *tmp_names = krealloc(p->sym_val_to_name[SYM_TYPES], 
+    tmp_names = krealloc(p->sym_val_to_name[SYM_TYPES], 
                                sizeof(char *) * new_value, GFP_KERNEL);
     if (!tmp_names) goto err;
     p->sym_val_to_name[SYM_TYPES] = tmp_names;
     p->sym_val_to_name[SYM_TYPES][new_value - 1] = name_copy;
 
-    void *tmp_structs = krealloc(p->type_val_to_struct, 
+    tmp_structs = krealloc(p->type_val_to_struct, 
                                  sizeof(struct type_datum *) * new_value, GFP_KERNEL);
     if (!tmp_structs) goto err;
     p->type_val_to_struct = tmp_structs;
     p->type_val_to_struct[new_value - 1] = type;
 
-    void *tmp_map = krealloc(p->type_attr_map_array, 
+    tmp_map = krealloc(p->type_attr_map_array, 
                              sizeof(struct ebitmap) * new_value, GFP_KERNEL);
     if (!tmp_map) goto err;
     p->type_attr_map_array = tmp_map;
     ebitmap_init(&p->type_attr_map_array[new_value - 1]);
 
-    rc = hashtab_insert(p->p_types.table, name_copy, type, sym_params);
+    rc = hashtab_insert(&p->p_types.table, name_copy, type, sym_params);
     if (rc) goto err;
 
     p->p_types.nprim++;
