04a13512e4ec32b67ad1b6b07235de494864d1f2
aqnya <siyueba73@gmail.com>
2026-04-19 00:32:12 +0800

    use ksu rules.

--- src/selinux/policy.c
+++ src/selinux/policy.c
@@ -33,120 +33,43 @@ struct sepolicy_group {
 	bool required;
 };
 
-static const struct sepolicy_rule global_file_access_rules[] = {
-	ALLOW(DOMAIN, NULL, "file", "read"),
-	ALLOW(DOMAIN, NULL, "file", "open"),
-	ALLOW(DOMAIN, NULL, "file", "getattr"),
-	ALLOW(DOMAIN, NULL, "file", "execute"),
-	ALLOW(DOMAIN, NULL, "file", "execute_no_trans"),
-	ALLOW(DOMAIN, NULL, "file", "map"),
-	ALLOW(DOMAIN, NULL, "dir", "read"),
-	ALLOW(DOMAIN, NULL, "dir", "open"),
-	ALLOW(DOMAIN, NULL, "dir", "search"),
-	ALLOW(DOMAIN, NULL, "dir", "getattr"),
-	ALLOW(DOMAIN, NULL, "lnk_file", "read"),
-	ALLOW(DOMAIN, NULL, "lnk_file", "getattr"),
-};
-
-static const struct sepolicy_rule pkg_rules[] = {
-	ALLOW(DOMAIN, "package_native_service", "service_manager", "find"),
-	ALLOW(DOMAIN, "activity_service", "service_manager", "find"),
-	ALLOW(DOMAIN, "system_server", NULL, NULL),
-	ALLOW(DOMAIN, "servicemanager", "fd", "use"),
-	ALLOW(DOMAIN, "domain", "fd", "use"),
-	ALLOW("domain",DOMAIN, "fd", "use"),
-	ALLOW(DOMAIN, "package_service", "service_manager", "find"),
-};
-
-static const struct sepolicy_rule transition_rules[] = {
-	ALLOW("shell", DOMAIN, "process", "transition"),
-	ALLOW("untrusted_app", DOMAIN, "process", "sigchld"),
-	ALLOW("untrusted_app", DOMAIN, "process", "setpgid"),
-	ALLOW("untrusted_app", DOMAIN, "process", "getpgid"),
-	ALLOW("untrusted_app", DOMAIN, "process", "signull"),
-};
-
-static const struct sepolicy_rule debug_rules[] = {
-	ALLOW(DOMAIN, "domain", "process", "ptrace"),
-	ALLOW(DOMAIN, "domain", "process", "signull"),
-	ALLOW(DOMAIN, "domain", "process", "signal"),
-	ALLOW(DOMAIN, "domain", "process", "sigkill"),
-	ALLOW(DOMAIN, "domain", "process", "getpgid"),
-	ALLOW(DOMAIN, "domain", "process", "setsched"),
-};
-
-static const struct sepolicy_rule fs_rules[] = {
-	ALLOW(DOMAIN, "device", "dir", "write"),
-	ALLOW(DOMAIN, "null_device", "chr_file", "read"),
-	ALLOW(DOMAIN, "null_device", "chr_file", "write"),
-	ALLOW(DOMAIN, "zero_device", "chr_file", "read"),
-	ALLOW(DOMAIN, "kmsg_device", "chr_file", "write"),
-	ALLOW(DOMAIN, "adb_data_file", "file", "write"),
-	ALLOW(DOMAIN, "adb_data_file", "file", "create"),
-};
-
-static const struct sepolicy_rule svc_rules[] = {
-	ALLOW(DOMAIN, "servicemanager", "service_manager", "list"),
-	ALLOW(DOMAIN, "servicemanager", "service_manager", "find"),
-	ALLOW(DOMAIN, "service_manager_type", "service_manager", "find"),
-};
-
-static const struct sepolicy_rule binder_rules[] = {
-	ALLOW(DOMAIN, "binder_device", "chr_file", NULL),
-	ALLOW(DOMAIN, NULL, "binder", NULL),
-	ALLOW("servicemanager", DOMAIN, "binder", NULL),
-	ALLOW("system_server", DOMAIN, NULL, NULL),
-	ALLOW("installd", DOMAIN, "binder", "call"),
-};
-
-static const struct sepolicy_rule prop_ext_rules[] = {
-	ALLOW(DOMAIN, "property_socket", "sock_file", "write"),
-	ALLOW(DOMAIN, "init", "unix_stream_socket", "connectto"),
-	ALLOW(DOMAIN, "default_prop", "property_service", "set"),
-	ALLOW(DOMAIN, "system_prop", "property_service", "set"),
-	ALLOW(DOMAIN, "exported_config_prop", "property_service", "set"),
-};
-
-static const struct sepolicy_rule net_rules[] = {
-	ALLOW(DOMAIN, DOMAIN, "udp_socket", NULL),
-	ALLOW(DOMAIN, DOMAIN, "tcp_socket", NULL),
-	ALLOW(DOMAIN, "node", "tcp_socket", "node_bind"),
-	ALLOW(DOMAIN, "port", "tcp_socket", "name_connect"),
-	ALLOW(DOMAIN, DOMAIN, "unix_dgram_socket", NULL),
-	ALLOW(DOMAIN, DOMAIN, "unix_stream_socket", NULL),
-};
-
-static const struct sepolicy_rule cap_rules[] = {
-	ALLOW(DOMAIN, DOMAIN, "capability", NULL),
-};
-
 static const struct sepolicy_rule su_rules[] = {
-	ALLOW(DOMAIN, DOMAIN, "fd", "use"),
-	ALLOW(DOMAIN, NULL, "fifo_file", NULL),
-	ALLOW(DOMAIN, "shell_data_file", "file", "write"),
-	ALLOW(DOMAIN, NULL, "process", NULL),
+	ALLOW(DOMAIN, NULL, NULL, NULL),
 };
 
-static const struct sepolicy_rule su_fix_rules[] = {
-	ALLOW(DOMAIN, "system_file", "file", "entrypoint"),
-	ALLOW(DOMAIN, "shell", "fd", "use"),
-	ALLOW("shell", DOMAIN, "fd", "use"),
-	ALLOW(DOMAIN, "devpts", "chr_file", "read"),
-	ALLOW(DOMAIN, "devpts", "chr_file", "write"),
-	ALLOW(DOMAIN, "devpts", "chr_file", "ioctl"),
-	ALLOW(DOMAIN, "devpts", "chr_file", "open"),
-	ALLOW("shell", DOMAIN, "process", "sigchld"),
-	ALLOW(DOMAIN, "untrusted_app_all_devpts", "chr_file", "write"),
-	ALLOW(DOMAIN, "untrusted_app_all_devpts", "chr_file", "read"),
-	ALLOW(DOMAIN, "untrusted_app_all_devpts", "chr_file", "open"),
-	ALLOW(DOMAIN, "untrusted_app_all_devpts", "chr_file", "ioctl"),
-	ALLOW(DOMAIN, "untrusted_app_all_devpts", "chr_file", "getattr"),
-	ALLOW(DOMAIN, DOMAIN, "lockdown", NULL),
-};
+static const struct sepolicy_rule ksu_rules[] = {
+   ALLOW("servicemanager", KERNEL_SU_DOMAIN, "dir", "search");
+    ALLOW("servicemanager", KERNEL_SU_DOMAIN, "dir", "read");
+    ALLOW("servicemanager", KERNEL_SU_DOMAIN, "file", "open");
+    ALLOW("servicemanager", KERNEL_SU_DOMAIN, "file", "read");
+    ALLOW("servicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
+    ALLOW("domain", KERNEL_SU_DOMAIN, "process", "sigchld");
 
-static const struct sepolicy_rule klog_rules[] = {
-	ALLOW(DOMAIN, "kernel", "system", "syslog_read"),
-	ALLOW(DOMAIN, "kmsg_device", "chr_file", NULL),
+    // allowLog
+    ALLOW("logd", KERNEL_SU_DOMAIN, "dir", "search");
+    ALLOW("logd", KERNEL_SU_DOMAIN, "file", "read");
+    ALLOW("logd", KERNEL_SU_DOMAIN, "file", "open");
+    ALLOW("logd", KERNEL_SU_DOMAIN, "file", "getattr");
+
+    // dumpsys, send fd
+    ALLOW("domain", KERNEL_SU_DOMAIN, "fd", "use");
+    ALLOW("domain", KERNEL_SU_DOMAIN, "fifo_file", "write");
+    ALLOW("domain", KERNEL_SU_DOMAIN, "fifo_file", "read");
+    ALLOW("domain", KERNEL_SU_DOMAIN, "fifo_file", "open");
+    ALLOW("domain", KERNEL_SU_DOMAIN, "fifo_file", "getattr");
+
+    // bootctl
+    ALLOW("hwservicemanager", KERNEL_SU_DOMAIN, "dir", "search");
+    ALLOW("hwservicemanager", KERNEL_SU_DOMAIN, "file", "read");
+    ALLOW("hwservicemanager", KERNEL_SU_DOMAIN, "file", "open");
+    ALLOW("hwservicemanager", KERNEL_SU_DOMAIN, "process", "getattr");
+
+    // Allow all binder transactions
+    ALLOW("domain", KERNEL_SU_DOMAIN, "binder", ALL);
+
+    // Allow system server kill su process
+    ALLOW("system_server", KERNEL_SU_DOMAIN, "process", "getpgid");
+    ALLOW("system_server", KERNEL_SU_DOMAIN, "process", "sigkill");
 };
 
 #define GROUP(_name, _rules, _required) \
@@ -154,19 +77,8 @@ static const struct sepolicy_rule klog_rules[] = {
       .required = (_required) }
 
 static const struct sepolicy_group policy_groups[] = {
-	GROUP("global_file_access", global_file_access_rules, true),
-	GROUP("package_manager", pkg_rules, true),
 	GROUP("su_basic", su_rules, true),
-	GROUP("su_fix", su_fix_rules, true),
-	GROUP("service", svc_rules, true),
-	GROUP("binder", binder_rules, true),
-	GROUP("prop_ext", prop_ext_rules, false),
-	GROUP("cap", cap_rules, true),
-	GROUP("fs_access", fs_rules, false),
-	GROUP("debug", debug_rules, false),
-	GROUP("net", net_rules, false),
-	GROUP("transition", transition_rules, true),
-	GROUP("klog", klog_rules, true),
+	GROUP("ksu_rules",ksu_rules,true),
 };
 
 static int apply_group(const struct sepolicy_group *grp)
