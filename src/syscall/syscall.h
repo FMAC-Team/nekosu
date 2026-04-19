3873704aa10cc6ba96fdd16f5cff85210b93d77d
aqnya <siyueba73@gmail.com>
2026-04-20 05:57:22 +0800

    fix ,

--- src/syscall/syscall.h
+++ src/syscall/syscall.h
@@ -2,4 +2,4 @@ int syscalltable_init(void);
 void syscalltable_exit(void);
 int hook_one(int nr, syscall_fn_t fn, syscall_fn_t *orig, const char *name);
 
-syscall_fn_t *syscall_table;
+extern syscall_fn_t *syscall_table;
