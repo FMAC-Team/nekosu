5d70fd84d5bae7568b0117f3b93467a2b14a3414
aqnya <siyueba73@gmail.com>
2026-04-21 02:33:48 +0800

    eliminate historical legacy

--- src/include/tracepoint.h
+++ src/include/tracepoint.h
@@ -1,14 +1,6 @@
 #ifndef TRACEPOINT_H
 #define TRACEPOINT_H
 
-#define REDIRECT_TARGET     "/data/adb/ncore"
-#define REDIRECT_TARGET_LEN (sizeof(REDIRECT_TARGET))
-#define SH_PATH             "/system/bin/sh"
-#define SH_PATH_LEN         (sizeof(SH_PATH))
-#define SU_PATH             "/system/bin/su"
-#define SU_PATH_LEN         (sizeof(SU_PATH))
-#define SCOPE_HASH_BITS 6
-
 u32 scope_lookup(uid_t uid);
 int fmac_scope_set(uid_t uid, u32 flags);
 void fmac_scope_clear(uid_t uid);
