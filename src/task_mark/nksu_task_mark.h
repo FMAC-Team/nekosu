4ad2a7ce7d963edc6520728685f8812ba995f075
aqnya <siyueba73@gmail.com>
2026-04-22 03:37:50 +0800

    opz.

--- src/task_mark/nksu_task_mark.h
+++ src/task_mark/nksu_task_mark.h
@@ -1,4 +1,8 @@
+#pragma once
+
 #include <linux/types.h>
+#include <linux/bitops.h>
+#include <linux/sched.h>
 
 #define NKSU_MARK_AUTHORIZED  BIT(0)
 #define NKSU_MARK_ROOT        BIT(1)
@@ -11,23 +15,18 @@
 #endif
 
 int  nksu_kabi_field_check(void);
+
 u32  nksu_task_get_mark(struct task_struct *task);
 bool nksu_task_check_mark(struct task_struct *task, u32 mark);
 void nksu_task_set_mark(struct task_struct *task, u32 mark);
 void nksu_task_clear_mark(struct task_struct *task, u32 mark);
+
 int  nksu_task_mark_init(void);
 void nksu_task_mark_exit(void);
 
-#define nksu_current_check_mark(mark) \
-    nksu_task_check_mark(current, mark)
-#define nksu_current_set_mark(mark) \
-    nksu_task_set_mark(current, mark)
-#define nksu_current_clear_mark(mark) \
-    nksu_task_clear_mark(current, mark)
-
 static __always_inline u32 *nksu_mark_ptr(struct task_struct *task)
 {
-	return (u32 *)&task->NKSU_KABI_FIELD;
+    return (u32 *)&task->NKSU_KABI_FIELD;
 }
 
 static __always_inline u32 nksu_task_get_mark_inline(struct task_struct *task)
