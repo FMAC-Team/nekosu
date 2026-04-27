0755eaccb423d7416b3b2b88ba3c7469e4f5f445
aqnya <siyueba73@gmail.com>
2026-04-28 04:08:08 +0800

    use profile.

--- src/include/ns.h
+++ src/include/ns.h
@@ -1,1 +1,5 @@
+#define NKSU_NS_INHERITED 1
+#define NKSU_NS_INDIVIDUAL 2
+#define NKSU_NS_GLOBAL 3
+
 int switch_to_init_ns(void);
--- src/profile/ns.h
+++
@@ -1,3 +0,0 @@
-#define NKSU_NS_INHERITED 1
-#define NKSU_NS_INDIVIDUAL 2
-#define NKSU_NS_GLOBAL 3
